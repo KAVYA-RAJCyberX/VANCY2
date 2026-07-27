57bbae37c517386efb265fd61c186983b3deab31
KAVYA-RAJCyberX <chouhankavyaraj721@gmail.com>
2026-07-27 21:21:25 +0530

    chore: add admin seed script for chouhankavyaraj721@gmail.com

---
+++ server/seedAdmin.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const connectDB = require('./src/config/db');
+const User = require('./src/models/User');
+
+const seedAdmin = async () => {
+  await connectDB();
+
+  const adminEmail = 'chouhankavyaraj721@gmail.com';
+
+  let admin = await User.findOne({ email: adminEmail });
+
+  if (admin) {
+    console.log('Admin user already exists. Updating role to super-admin just in case...');
+    admin.role = 'super-admin';
+    admin.isAdmin = true;
+    await admin.save();
+    console.log('Admin account ready.');
+  } else {
+    admin = await User.create({
+      name: 'Vancy Super Admin',
+      email: adminEmail,
+      password: 'vancyadmin123!', // This will be hashed automatically by the User schema pre-save hook
+      role: 'super-admin',
+      isAdmin: true,
+      twoFactorEnabled: false // Forces the QR code to appear on first login
+    });
+    console.log('Super Admin account created successfully!');
+  }
+
+  console.log(`\n--- ADMIN CREDENTIALS ---`);
+  console.log(`URL: http://localhost:5173/admin/login`);
+  console.log(`Email: ${adminEmail}`);
+  console.log(`Password: vancyadmin123!\n`);
+
+  process.exit(0);
+};
+
+seedAdmin();

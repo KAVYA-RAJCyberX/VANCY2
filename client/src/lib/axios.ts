43a224265b905af1d4d0b1d584438ad073acf84b
KAVYA-RAJCyberX <chouhankavyaraj721@gmail.com>
2026-08-11 16:45:41 +0530

    fix: return token on auth and add withCredentials to axios

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/api', // Pointing to the Express backend
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -10,12 +10,13 @@ const authUser = async (req, res) => {
     
     const user = await User.findOne({ email });
     if (user && (await user.matchPassword(password))) {
-      generateToken(res, user._id);
+      const token = generateToken(res, user._id);
       res.json({
         _id: user._id,
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
+        token: token,
       });
     } else {
       res.status(401).json({ message: 'Invalid email or password' });
@@ -41,12 +42,13 @@ const registerUser = async (req, res) => {
     const user = await User.create({ name, email, password });
 
     if (user) {
-      generateToken(res, user._id);
+      const token = generateToken(res, user._id);
       res.status(201).json({
         _id: user._id,
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
+        token: token,
       });
     } else {
       res.status(400).json({ message: 'Invalid user data' });
